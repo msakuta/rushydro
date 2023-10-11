66d22e39bdf67690e98018cdfdef99bca5bb9a27
msakuta <masahiro.sakuta@gmail.com>
2023-10-11 22:58:48 +0900

    Support Wasm build

---
+++ README.md
@@ -0,0 +1,25 @@
+# rushydro
+
+Fluid simulation with particles
+
+## How to run natively
+
+```
+cargo r
+```
+
+
+## How to build a WebAssembly version
+
+Install trunk by 
+
+```
+cargo install --locked trunk
+```
+
+and run
+
+```
+trunk build --release
+```
+
--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,29 @@ fn main() {
     )
     .unwrap();
 }
+
+// when compiling to web using trunk.
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Make sure panics are logged using `console.error`.
+    console_error_panic_hook::set_once();
+
+    // Redirect tracing to console.log and friends:
+    tracing_wasm::set_as_global_default();
+
+    let mut web_options = eframe::WebOptions::default();
+
+    // We insist to use light theme, because the canvas color is designed to work with light background.
+    web_options.follow_system_theme = false;
+    web_options.default_theme = eframe::Theme::Light;
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::start_web(
+            "the_canvas_id", // hardcode it
+            web_options,
+            Box::new(|cc| Box::new(RusHydroApp::new())),
+        )
+        .await
+        .expect("failed to start eframe");
+    });
+}
